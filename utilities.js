3650bbe8eb77efa21ea097b369902e95fae47df7
ktapia576 <ktapia576@hotmail.com>
2019-10-15 19:37:23 -0400

    Add on change event listner, to wait for file upload

--- utilities.js
+++ utilities.js
@@ -1,15 +1,19 @@
 const loadFile = () => { 
     console.log('hi');
-    var csvFile = document.getElementById("loadcsv").files[0];   // get first file only
+    var csvFile = document.getElementById("loadFile").files[0];   // get first file only
     console.log(csvFile);
 }
 
-$( "#loadFile" ).click(function( e ) {  // add event listener to <a> element
-    $( "#loadcsv" ).trigger("click");     // activate event handler on #loadcsv
+$( "#loadFileBtn" ).click(function( e ) {  // add event listener to <a> element
+    $( "#loadFile" ).trigger("click");     // activate event handler on #loadcsv
   });
 
   
-$( "#loadcsv" ).click(function( e ) {   // add event listener to <input type="file"> element
+$( "#loadFile" ).click(function( e ) {   // add event listener to <input type="file"> element
     e.stopPropagation();    // stop bubbling from occuring
-    loadFile();
   });
+
+$("#loadFile").change(function(e){  // when value of input changes (once user uploads file), do this
+  alert('File Selected!');
+  loadFile();
+});
