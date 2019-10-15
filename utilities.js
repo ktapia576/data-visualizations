3849a840443898bbd2d933b7d45b3874c4bee21e
ktapia576 <ktapia576@hotmail.com>
2019-10-15 02:20:32 -0400

    Fixed bubbling bug and event listeners

--- utilities.js
+++ utilities.js
@@ -1,7 +1,15 @@
 const loadFile = () => { 
-    document.getElementById('loadcsv').click(); // open file explorer to choose file
     console.log('hi');
-
+    var csvFile = document.getElementById("loadcsv").files[0];   // get first file only
+    console.log(csvFile);
 }
 
-document.getElementById('loadFile').addEventListener('click', loadFile);    // add onclick listener for loadfile button
+$( "#loadFile" ).click(function( e ) {  // add event listener to <a> element
+    $( "#loadcsv" ).trigger("click");     // activate event handler on #loadcsv
+  });
+
+  
+$( "#loadcsv" ).click(function( e ) {   // add event listener to <input type="file"> element
+    e.stopPropagation();    // stop bubbling from occuring
+    loadFile();
+  });
