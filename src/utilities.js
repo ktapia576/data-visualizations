5bdfb5f6a5f2aee531116e521042d584e0121c52
ktapia576 <ktapia576@hotmail.com>
2019-10-16 02:42:16 -0400

    Format code and add error handle

--- src/utilities.js
+++ src/utilities.js
@@ -2,25 +2,24 @@ const loadFile = () => {
   const csvFile = document.getElementById("loadFile").files[0];   // get first file only
   console.log(csvFile);
 
-  console.log(navigator.deviceMemory);  // Only works on Chrome
+  //console.log(navigator.deviceMemory);  // Only works on Chrome
 
   // Parse local CSV file
   Papa.parse(csvFile, {
-    complete: function(results) {   // The callback to execute when parsing is complete
-      console.log("Finished:", results.data);
-    }
+    complete: results => { console.log("Finished:", results.data); }, // Callback to execute when parsing complete
+    error: error => { console.log(error); } // Callback to execute if FileReader encounters an error.
   });
 }
 
-$( "#loadFileBtn" ).click(function( e ) {  // add event listener to <a> element
+$( "#loadFileBtn" ).click( e => {  // add event listener to <a> element
   $( "#loadFile" ).trigger("click");     // activate event handler on #loadcsv
 });
 
   
-$( "#loadFile" ).click(function( e ) {   // add event listener to <input type="file"> element
+$( "#loadFile" ).click( e => {   // add event listener to <input type="file"> element
   e.stopPropagation();    // stop bubbling from occuring
 });
 
-$("#loadFile").change(function(e){  // when value of input changes (once user uploads file), do this [change event is sent to an element when its value changes]
+$("#loadFile").change( e => {  // when value of input changes (once user uploads file), do this [change event is sent to an element when its value changes]
   loadFile();
 });
