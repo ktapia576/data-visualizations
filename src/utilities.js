0ac3bdc2829311ebfbc3cb30b845a7ed59e0d4cd
ktapia576 <ktapia576@hotmail.com>
2019-10-16 01:01:43 -0400

    Add Papa Parse library and extract data from csv file uploaded from user

--- src/utilities.js
+++ src/utilities.js
@@ -1,21 +1,27 @@
 const loadFile = () => { 
-    console.log('hi');
-    var csvFile = document.getElementById("loadFile").files[0];   // get first file only
-    console.log(csvFile);
-    var data = $.csv.toObjects(csvFile);
-    console.log(data);
+  var csvFile = document.getElementById("loadFile").files[0];   // get first file only
+  console.log(csvFile);
+  
+  var reader = new FileReader();
+  var data;
+
+  reader.readAsText(csvFile);   // result attribute contains the contents of the file as a text string. DOES THIS IN ASYNC FASHION!! (careful for bugs)
+  reader.onloadend = function(){  // This event is triggered each time the reading operation is completed (either in success or failure).
+    data = reader.result;   // result property returns the file's contents. Only after read operation
+    var parsed = Papa.parse(data);
+    console.log(parsed);
+  }
 }
 
 $( "#loadFileBtn" ).click(function( e ) {  // add event listener to <a> element
-    $( "#loadFile" ).trigger("click");     // activate event handler on #loadcsv
-  });
+  $( "#loadFile" ).trigger("click");     // activate event handler on #loadcsv
+});
 
   
 $( "#loadFile" ).click(function( e ) {   // add event listener to <input type="file"> element
-    e.stopPropagation();    // stop bubbling from occuring
-  });
+  e.stopPropagation();    // stop bubbling from occuring
+});
 
 $("#loadFile").change(function(e){  // when value of input changes (once user uploads file), do this [change event is sent to an element when its value changes]
-  alert('File Selected!');
   loadFile();
 });
