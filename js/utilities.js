d7b923048130fd178087832dfa52753d3b069053
ktapia576 <ktapia576@hotmail.com>
2019-10-20 18:53:45 -0400

    Add Client Info Modal

--- js/utilities.js
+++ js/utilities.js
@@ -138,3 +138,22 @@ $('#userInfoBtn').click( e => {
 
   document.getElementById('userInfoModal').style.display='block'; // show login modal
 });
+
+$('#clientBtn').click( e => {
+  e.preventDefault(); // default action of an element from happening
+  var browser = navigator.userAgent;  // Get user's browser
+  var os = navigator.platform; 
+  var version = navigator.appVersion;
+  var vendor = navigator.vendor;  // Mozilla Firefox returns empty string
+  var cookie = navigator.cookieEnabled;
+  var java= navigator.javaEnabled();
+
+  var clientInfo = `<b>Browser:</b> ${browser}, <b>${vendor}</b></br>
+    <b>OS Platform:</b> ${os}</br>
+    <b>Browser Version:</b> ${version}</br>
+    <b>Cookies Enabled:</b> ${cookie}</br>
+    <b>Java Enabled:</b> ${java}</br>`;
+  
+  document.getElementById('clientUserInfo').innerHTML = clientInfo; // innerHTML returns HTML, textContent has better performance because its value is not parsed as HTML
+  document.getElementById('clientInfoModal').style.display='block'; // show login modal
+});
